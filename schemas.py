98a795f0b596a1a38a78f5c04076e9f9dacf7967
Faraz <146446223+CyberCryptik@users.noreply.github.com>
2025-08-15 14:33:05 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,27 +1,13 @@
-"""from pydantic import BaseModel
-from typing import List, Dict, Any
-
-class AggregateRequest(BaseModel):
-    # added this to specify that any collection can be used (previously only the "comments" collection):
-    collection: str
-    pipeline: List[Dict[str, Any]]
-"""
-
-"""from pydantic import BaseModel
-from typing import List, Dict, Any, Union
-
-class AggregateRequest(BaseModel):
-    collection: str
-    pipeline: Union[List[Dict[str, Any]], List[str]]"""
-
 # schemas.py
 from pydantic import BaseModel
 from typing import List, Dict, Any, Union, Optional
 
 class AggregateRequest(BaseModel):
+    # db_name is optional. If not provided, it will be inferred.
     db_name: Optional[str] = None
     collection: str
-    pipeline: Union[List[Dict[str, Any]], List[str]]
+    pipeline: Union[List[Dict[str, Any]], List[str]] # Supports list of dicts or list of JSON strings
 
 class SchemaRequest(BaseModel):
+    # db_name is optional. If not provided, it will default to DB_NAME from environment.
     db_name: Optional[str] = None
